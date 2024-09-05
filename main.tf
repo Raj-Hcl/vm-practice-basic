c6aff68ded6220f12a0e3abc3dcd0e7906e47281
Raj-Hcl <raj.tripathi@hcltech.com>
2024-09-05 14:40:38 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "google_compute_instance" "myvm" {
+  name = var.myinstance
+  machine_type = var.machine
+  zone = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = var.myimage
+    }
+ }
+ network_interface {
+   network = "default"
+ }
+      
+    
